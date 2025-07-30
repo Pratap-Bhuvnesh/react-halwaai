59d283e82b2516922b68c11340cce4ac034c03d4
Bhuvnesh Pratap <pratapp.singh4@gmail.com>
2025-07-30 12:41:13 +0530

    check commit

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -43,7 +43,7 @@ export default function Contact() {
                             </div>
 
                             <div className="flex items-center mt-4 text-gray-600">
-                                <svg
+                                <svg 
                                     fill="none"
                                     stroke="currentColor"
                                     stroke-linecap="round"
